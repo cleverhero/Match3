598226ec8a923bb62bc24c1d563996b74c11e18b
Oleg Prikhodko <oldmpr@rambler.ru>
2017-05-23 05:55:16 +1000

    Improve GameForm
    Add render picturebox with game objects, new class - game field

--- GameForm.Designer.cs
+++ GameForm.Designer.cs
@@ -28,34 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.TimeLabel = new System.Windows.Forms.Label();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.GameField = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.GameField)).BeginInit();
             this.SuspendLayout();
             // 
-            // button
+            // TimeLabel
             // 
-            this.button.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button.Location = new System.Drawing.Point(145, 118);
-            this.button.Name = "button";
-            this.button.Size = new System.Drawing.Size(157, 95);
-            this.button.TabIndex = 0;
-            this.button.Text = "Gamed";
-            this.button.UseVisualStyleBackColor = true;
-            this.button.Click += new System.EventHandler(this.button_Click);
+            this.TimeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TimeLabel.Location = new System.Drawing.Point(12, 9);
+            this.TimeLabel.Name = "TimeLabel";
+            this.TimeLabel.Size = new System.Drawing.Size(233, 47);
+            this.TimeLabel.TabIndex = 0;
+            this.TimeLabel.Text = "Time:";
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ScoreLabel.Location = new System.Drawing.Point(398, 9);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(281, 47);
+            this.ScoreLabel.TabIndex = 1;
+            this.ScoreLabel.Text = "Score:";
+            // 
+            // GameField
+            // 
+            this.GameField.Location = new System.Drawing.Point(12, 59);
+            this.GameField.Name = "GameField";
+            this.GameField.Size = new System.Drawing.Size(670, 667);
+            this.GameField.TabIndex = 2;
+            this.GameField.TabStop = false;
+            this.GameField.Paint += new System.Windows.Forms.PaintEventHandler(this.GameField_Paint);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // GameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(465, 366);
-            this.Controls.Add(this.button);
+            this.ClientSize = new System.Drawing.Size(691, 738);
+            this.Controls.Add(this.GameField);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.TimeLabel);
             this.Name = "GameForm";
             this.Text = "Game";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GameForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.GameField)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label TimeLabel;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.PictureBox GameField;
+        private System.Windows.Forms.Timer timer;
     }
 }
--- GameForm.cs
+++ GameForm.cs
@@ -8,18 +8,131 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+public enum FigureType { Rectangle, Trianlge }
+
 namespace Match3
 {
     public partial class GameForm : Form
     {
+        const int count = 10;
+
+        private uint score;
+        private uint time;
+        private Field[][] fields;
+
         public GameForm()
         {
+            score = 0;
+            time = 60;
+
             InitializeComponent();
+            ScoreLabel.Text = "Score: " + Convert.ToString(score);
+            TimeLabel.Text = "Time: " + Convert.ToString(time);
+
+            fields = new Field[count][];
+            for (int i = 0; i < count; i++)
+            {
+                fields[i] = new Field[count];
+                for (int j = 0; j < count; j++)
+                {
+                    fields[i][j] = new Field(FigureType.Trianlge, Color.Blue);
+                }
+            }
+
+            timer.Start();
         }
 
         private void button_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void GameField_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics paintbox = e.Graphics;
+            float width = this.GameField.Width / count;
+            Point size = new Point(Convert.ToInt32(width), Convert.ToInt32(width));
+            
+            for (int i = 0; i < count; i++)
+                for (int j = 0; j < count; j++)
+                {
+                    Point position = new Point(Convert.ToInt32(width * i), Convert.ToInt32(width * j));
+                    fields[i][j].Draw(paintbox, position, size);
+                }
+
+            Pen pen = new Pen(Color.Black, 2.0f);
+
+            for (int i = 0; i <= count; i++)
+            {
+                Point p1 = new Point(Convert.ToInt32(width * i), 0);
+                Point p2 = new Point(Convert.ToInt32(width * i), this.GameField.Width - 1);
+                paintbox.DrawLine(pen, p1, p2);
+
+                p1 = new Point(0, Convert.ToInt32(width * i));
+                p2 = new Point(this.GameField.Width - 1, Convert.ToInt32(width * i));
+                paintbox.DrawLine(pen, p1, p2);
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            time -= 1;
+            TimeLabel.Text = "Time: " + Convert.ToString(time);
+            if (time == 0) Close();
+        }
+
+        private void GameForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer.Stop();
+        }
+    }
+
+    public class Field
+    {
+        public float Angle
+        {
+            get { return angle; }
+            set { angle = value; }
+        }
+        public Point Shift
+        {
+            get { return shift; }
+            set { shift = value; }
+        }
+
+        private float angle;
+        private Point shift;
+        private FigureType type;
+        private Color color;
+
+        public Field(FigureType _type, Color _color)
+        {
+            angle = 0.0f;
+            shift = new Point(0, 0);
+            type = _type;
+            color = _color;
+        }
+
+        public void Draw(Graphics g, Point pos, Point size)
+        {
+            g.TranslateTransform(shift.X, shift.Y);
+            g.RotateTransform(angle);
+
+            SolidBrush brush = new SolidBrush(Color.FromArgb(255, color));
+            if (type == FigureType.Rectangle)
+            {
+                g.FillRectangle(brush, new RectangleF(pos.X + 8, pos.Y + 8, size.X - 8, size.Y - 8));
+            }
+            else
+            {
+                Point[] points = new Point[3]
+                {
+                    new Point(pos.X + 8,          pos.Y + size.Y - 8),
+                    new Point(pos.X + size.X / 2, pos.Y + 8),
+                    new Point(pos.X + size.X - 8, pos.Y + size.Y - 8)
+                };
+                g.FillPolygon(brush, points);
+            }
         }
     }
 }
